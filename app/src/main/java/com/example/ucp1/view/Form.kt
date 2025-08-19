9b7d82bcf93ff51fa214cfda6bb9c3ed1ba88521
Uknown <abriansyahadam5@gmail.com>
2025-08-19 20:49:15 +0700

    add variable remember state

--- app/src/main/java/com/example/ucp1/view/Form.kt
+++ app/src/main/java/com/example/ucp1/view/Form.kt
@@ -38,4 +38,10 @@ fun Formulir(
     pilihanJK: List<String>,
     onSubmitBtnClick: (MutableList<String>) -> Unit,
     modifier: Modifier = Modifier
-) {}
+) {
+
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
+}
