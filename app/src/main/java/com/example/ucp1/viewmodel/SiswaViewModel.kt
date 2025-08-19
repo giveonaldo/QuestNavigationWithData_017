ef4500a0b4fc7ad07015df388f3c8a8b3e61c2b5
Uknown <abriansyahadam5@gmail.com>
2025-08-19 20:53:33 +0700

    add class SiswaViewModel

--- app/src/main/java/com/example/ucp1/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/ucp1/viewmodel/SiswaViewModel.kt
@@ -7,3 +7,13 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(DataSiswa())
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama = ls[0], gender = ls[1], alamat = ls[2])
+        }
+    }
+}
